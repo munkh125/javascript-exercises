c3014d17afd47252d65ca91d7e5698c5fe7217ae
munkh125 <munkh125@gmail.com>
2024-03-22 17:01:08 +0800

    done 1,2,3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const arr = input.split("");
+    input = "";
+    for(let i = arr.length-1; i >= 0; i--){
+        input +=  arr[i];
+    }
+    return input;
 };
 
 // Do not edit below this line
