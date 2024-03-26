28a839bda340da90101076825c599a5e1c64fcd1
munkh125 <munkh125@gmail.com>
2024-03-26 12:26:09 +0800

    done 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  const newArray = [];
+  array.forEach((element) => {
+    if (!args.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
-
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
